37d1e70700aef08342716644fd9ea1110019b7ff
fkammw <123600013+fkammw@users.noreply.github.com>
2023-09-14 17:03:56 -0600

    demos for WebDev 2

---
+++ app/week2/event-list.js
@@ -0,0 +1,11 @@
+
+export default function EventList() {
+    return (
+        <div>
+            <h1> Event List </h1>
+            <h2>Community Picnic</h2>
+            <p>date: 2023-09-15</p>
+            <p>location: Nose Hill Park</p>
+        </div>
+    );
+    }
---
+++ app/week2/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import EventList from "./event-list";
+
+
+export default function Page() {
+    return (
+        <main>
+            <h1> Hello Week 2 </h1>
+            <Link href="/">Back to Home</Link>
+            <EventList/>
+        </main>
+    );
+   
+}
